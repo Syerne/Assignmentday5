333bdf03c876cd22bf3141958d2a286a50bd6e62
Syerne <shubhamyerne1994@gmail.com>
2022-05-22 13:56:47 +0530

    Added: 5 two digit random number sum & average

--- totalwork.sh
+++ totalwork.sh
@@ -13,3 +13,18 @@ Dice2=$(( 1 + RANDOM%6 ))
 echo $Dice2
 AddResult=$(( Dice1 + Dice2 ))
 echo $AddResult
+
+num1=$(( 10 + RANDOM%90 ))
+echo $num1
+num2=$(( 10 + RANDOM%90 ))
+echo $num2
+num3=$(( 10 + RANDOM%90 ))
+echo $num3
+num4=$(( 10 + RANDOM%90 ))
+echo $num4
+num5=$(( 10 + RANDOM%90 ))
+echo $num5
+sum=$(( num1 + num2 + num3 + num4 + num5 ))
+echo $sum
+avg=$(( sum / 5 ))
+echo $avg
