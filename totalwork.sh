6e3a90766bbc359d6e7046d2d94dd457ec93e059
Syerne <shubhamyerne1994@gmail.com>
2022-05-23 18:08:55 +0530

    Modify: all program

--- totalwork.sh
+++ totalwork.sh
@@ -1,49 +1,48 @@
 read -p "welcome to assign work; type question number: " day
-echo $day
 
-RandomSingleDigitNumber=$(( RANDOM%10 ))
-echo $RandomSingleDigitNumber
+SingleDigitNumber=$(( RANDOM%10 ))
+echo "Random single digit is: " $SingleDigitNumber
 
 DiceRoll=$(( 1 + RANDOM%6 ))
-echo $DiceRoll
+echo "Dice number was: " $DiceRoll
 
 Dice1=$(( 1 + RANDOM%6 ))
-echo $Dice1
+echo "dice1 num: " $Dice1
 Dice2=$(( 1 + RANDOM%6 ))
-echo $Dice2
+echo "dice2 num: " $Dice2
 AddResult=$(( Dice1 + Dice2 ))
-echo $AddResult
+echo "sum of dice1 & dice2 is: " $AddResult
 
 num1=$(( 10 + RANDOM%90 ))
-echo $num1
+echo "random num1 is: " $num1
 num2=$(( 10 + RANDOM%90 ))
-echo $num2
+echo "random num2 is: " $num2
 num3=$(( 10 + RANDOM%90 ))
-echo $num3
+echo "random num3 is: " $num3
 num4=$(( 10 + RANDOM%90 ))
-echo $num4
+echo "random num4 is: " $num4
 num5=$(( 10 + RANDOM%90 ))
-echo $num5
+echo "random num5 is: " $num5
 sum=$(( num1 + num2 + num3 + num4 + num5 ))
-echo $sum
+echo "random num sum is: " $sum
 avg=$(( sum / 5 ))
-echo $avg
+echo "random num average is: " $avg
 
-read -p "enter value in inches" userinputrequired
-echo $userinputrequired
+read -p "enter value in inches: " userinput
+
 #1ft=12 inch
-feet=$(( userinputrequired / 12 ))
-echo $feet
-echo $userinputrequired==$feet
+feet=$(( userinput / 12 ))
+echo "feet: " $feet
+echo "inches convert into feet is: " $userinputrequired==$feet
 
 
-read -p "enter plot length in feet" length
-echo $length
-read -p "enter plot breath in feet" breath
-echo $breath
+read -p "enter plot length in feet: " length
+
+read -p "enter plot breath in feet: " breath
+
 area=$(( length * breath ))
-echo $area
+echo "area of plot is: " $area
 #1meter=3.28 approx 3.3
 areameter=$(( area/3.3 ))
 echo $areameter
-echo $area==$areameter
+echo $area=$areameter
